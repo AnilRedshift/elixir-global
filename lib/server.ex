43825f855d20f005760dbd6d2827398c61972e29
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-05-25 13:22:42 -0700

    Implement get and set

--- lib/server.ex
+++ lib/server.ex
@@ -12,12 +12,15 @@ defmodule Modglobal.Server do
 
   @impl true
   def handle_call({:get, module: module, key: key, default: default}, _from, state) do
-    value = default
-    {:reply, default, state}
+    value = state
+    |> Map.get(module, %{})
+    |> Map.get(key, default)
+    {:reply, value, state}
   end
 
   @impl true
   def handle_call({:set, module: module, key: key, value: value}, _from, state) do
+    state = put_in(state, [Access.key(module, %{}), key], value)
     {:reply, :ok, state}
   end
 end
--- test/modglobal_test.exs
+++ test/modglobal_test.exs
@@ -2,6 +2,7 @@ defmodule DummyModule do
   use Modglobal
   def get(key), do: get_global(key)
   def get(key, default), do: get_global(key, default)
+  def set(key, value), do: set_global(key, value)
 end
 
 defmodule ModglobalTest do
@@ -16,5 +17,19 @@ defmodule ModglobalTest do
 
   test "get returns the passed in default" do
     assert DummyModule.get("test", "cat") == "cat"
+  end
+
+  test "get returns nil if a default isn't present" do
+    assert DummyModule.get("test") == nil
+  end
+
+  test "set and retreive the value" do
+    DummyModule.set("test", "cat")
+    assert DummyModule.get("test") == "cat"
+  end
+
+  test "set and retrieve nil as a value" do
+    DummyModule.set("test", nil)
+    assert DummyModule.get("test", "cat") == nil
   end
 end
