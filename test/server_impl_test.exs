5fd73e68d94a885fedf23cba5b1bc0867828defb
Anil Kulkarni <akulkarni@avvo.com>
2018-05-29 15:57:56 -0700

    Fix impl tests to only test the implementation

--- test/server_impl_test.exs
+++ test/server_impl_test.exs
@@ -1,20 +1,10 @@
-defmodule DummyModule do
-  use Modglobal, public: true
-end
-
-defmodule DummyPrivateModule do
-  use Modglobal
-  def delete(key), do: delete_global(key)
-  def get(key), do: get_global(key)
-  def has?(key), do: has_global?(key)
-  def get(key, default), do: get_global(key, default)
-  def set(key, value), do: set_global(key, value)
-end
-
-defmodule ModglobalTest do
+defmodule Server.ImplTest do
 
   use ExUnit.Case, async: true
-  doctest Modglobal
+  alias Modglobal.Server.Impl
+
+  defmodule DummyModule do end
+  defmodule OtherDummyModule do end
 
   setup_all do
     old_env = Application.get_env(:modglobal, :impl)
@@ -31,78 +21,52 @@ defmodule ModglobalTest do
 
   describe "get" do
     test "returns the passed in default" do
-      assert DummyModule.get_global("test", "cat") == "cat"
-    end
-
-    test "returns nil if a default isn't present" do
-      assert DummyModule.get_global("test") == nil
+      assert Impl.get(module: DummyModule, key: "test", default: "cat") == "cat"
     end
   end
 
   describe "set" do
     test "a key to a value" do
-      DummyModule.set_global("test", "cat")
-      assert DummyModule.get_global("test") == "cat"
+      Impl.set(module: DummyModule, key: "test", value: "cat")
+      assert Impl.get(module: DummyModule, key: "test", default: nil) == "cat"
     end
 
     test "sets nil to a value" do
-      DummyModule.set_global("test", nil)
-      assert DummyModule.get_global("test", "cat") == nil
+      Impl.set(module: DummyModule, key: "test", value: nil)
+      assert Impl.get(module: DummyModule, key: "test", default: "cat") == nil
     end
   end
 
   describe "has?" do
     test "returns false when not present" do
-      assert DummyModule.has_global?("test") == false
+      assert Impl.has?(module: DummyModule, key: "test") == false
     end
 
     test "returns true when present" do
-      DummyModule.set_global("test", "cat")
-      assert DummyModule.has_global?("test") == true
+      Impl.set(module: DummyModule, key: "test", value: "cat")
+      assert Impl.has?(module: DummyModule, key: "test") == true
     end
   end
 
   describe "delete" do
     test "no-ops if not present" do
-      assert DummyModule.delete_global("test") == nil
-      assert DummyModule.has_global?("test") == false
+      assert Impl.delete(module: DummyModule, key: "test") == nil
+      assert Impl.has?(module: DummyModule, key: "test") == false
     end
 
     test "removes the key" do
-      DummyModule.set_global("test", "cat")
-      assert DummyModule.delete_global("test") == "cat"
-      assert DummyModule.has_global?("test") == false
-    end
-  end
-
-  describe "private api" do
-    test "make sure none of the methods are public" do
-      global_functions = DummyPrivateModule.__info__(:functions)
-      |> Enum.unzip
-      |> elem(0)
-      |> Enum.map(&Atom.to_string/1)
-      |> Enum.filter(&String.contains?(&1, "global"))
-      assert global_functions == []
-    end
-
-    test "ensure that private methods work properly" do
-      assert DummyPrivateModule.has?("test") == false
-      assert DummyPrivateModule.get("test") == nil
-      assert DummyPrivateModule.get("test", "cats") == "cats"
-      assert DummyPrivateModule.set("test", "cats") == nil
-      assert DummyPrivateModule.get("test") == "cats"
-      assert DummyPrivateModule.has?("test") == true
-      assert DummyPrivateModule.delete("test") == "cats"
-      assert DummyPrivateModule.has?("test") == false
+      Impl.set(module: DummyModule, key: "test", value: "cat")
+      assert Impl.delete(module: DummyModule, key: "test") == "cat"
+      assert Impl.has?(module: DummyModule, key: "test") == false
     end
   end
 
   describe "uniqueness" do
     test "make sure that changing one modules variables doesn't affect the other" do
-      DummyModule.set_global("test", "dogs")
-      DummyPrivateModule.set("test", "cats")
-      assert DummyModule.get_global("test") == "dogs"
-      assert DummyPrivateModule.get("test") == "cats"
+      Impl.set(module: DummyModule, key: "test", value: "dogs")
+      Impl.set(module: OtherDummyModule, key: "test", value: "cats")
+      assert Impl.get(module: DummyModule, key: "test", default: nil) == "dogs"
+      assert Impl.get(module: OtherDummyModule, key: "test", default: nil) == "cats"
     end
   end
 end
