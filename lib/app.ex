7822a0309e30a9806d2d38e6a18d31c5a39996ea
Anil Kulkarni <anil.kulkarni@outlook.com>
2018-07-13 01:02:35 -0700

    do not rely on mix.env for app children

--- lib/app.ex
+++ lib/app.ex
@@ -4,17 +4,10 @@ defmodule Modglobal.App do
   @impl true
   @doc false
   def start(_type, _args) do
-    Supervisor.start_link(children(Mix.env), strategy: :one_for_one)
+    Supervisor.start_link(children(), strategy: :one_for_one)
   end
 
-  defp children(env) when env == :test do
-    case Modglobal.Server.impl() do
-      Modglobal.Server.Impl -> children(:dev)
-      _ -> []
-    end
-  end
-
-  defp children(_env) do
+  defp children() do
     [{Modglobal.Server.Impl, name: Modglobal.Server.Impl}]
   end
 end
--- test/server_impl_test.exs
+++ test/server_impl_test.exs
@@ -7,19 +7,6 @@ defmodule Server.ImplTest do
   defmodule DummyModule do end
   defmodule OtherDummyModule do end
 
-  setup_all do
-    old_env = Application.get_env(:modglobal, :impl)
-    Application.put_env(:modglobal, :impl, Modglobal.Server.Impl)
-    on_exit fn ->
-      Application.put_env(:modglobal, :impl, old_env)
-    end
-  end
-
-  setup do
-    start_supervised!({Modglobal.Server.Impl, name: Modglobal.Server.Impl})
-    :ok
-  end
-
   describe "get" do
     test "returns the passed in default" do
       assert Impl.get(module: DummyModule, key: "test", default: "cat") == "cat"
@@ -28,57 +15,57 @@ defmodule Server.ImplTest do
 
   describe "set" do
     test "a key to a value" do
-      Impl.set(module: DummyModule, key: "test", value: "cat")
-      assert Impl.get(module: DummyModule, key: "test", default: nil) == "cat"
+      Impl.set(module: DummyModule, key: "test2", value: "cat")
+      assert Impl.get(module: DummyModule, key: "test2", default: nil) == "cat"
     end
 
     test "sets nil to a value" do
-      Impl.set(module: DummyModule, key: "test", value: nil)
-      assert Impl.get(module: DummyModule, key: "test", default: "cat") == nil
+      Impl.set(module: DummyModule, key: "test3", value: nil)
+      assert Impl.get(module: DummyModule, key: "test3", default: "cat") == nil
     end
   end
 
   describe "has?" do
     test "returns false when not present" do
-      assert Impl.has?(module: DummyModule, key: "test") == false
+      assert Impl.has?(module: DummyModule, key: "test4") == false
     end
 
     test "returns true when present" do
-      Impl.set(module: DummyModule, key: "test", value: "cat")
-      assert Impl.has?(module: DummyModule, key: "test") == true
+      Impl.set(module: DummyModule, key: "test5", value: "cat")
+      assert Impl.has?(module: DummyModule, key: "test5") == true
     end
   end
 
   describe "increment" do
     test "starts with 0 when not defined" do
-      assert Impl.increment(module: DummyModule, key: "test") == 0
+      assert Impl.increment(module: DummyModule, key: "test6") == 0
     end
 
     test "Subsequent calls return 1" do
-      Impl.increment(module: DummyModule, key: "test")
-      assert Impl.increment(module: DummyModule, key: "test") == 1
+      Impl.increment(module: DummyModule, key: "test7")
+      assert Impl.increment(module: DummyModule, key: "test7") == 1
     end
   end
 
   describe "delete" do
     test "no-ops if not present" do
-      assert Impl.delete(module: DummyModule, key: "test") == nil
-      assert Impl.has?(module: DummyModule, key: "test") == false
+      assert Impl.delete(module: DummyModule, key: "test8") == nil
+      assert Impl.has?(module: DummyModule, key: "test8") == false
     end
 
     test "removes the key" do
-      Impl.set(module: DummyModule, key: "test", value: "cat")
-      assert Impl.delete(module: DummyModule, key: "test") == "cat"
-      assert Impl.has?(module: DummyModule, key: "test") == false
+      Impl.set(module: DummyModule, key: "test9", value: "cat")
+      assert Impl.delete(module: DummyModule, key: "test9") == "cat"
+      assert Impl.has?(module: DummyModule, key: "test9") == false
     end
   end
 
   describe "uniqueness" do
     test "make sure that changing one modules variables doesn't affect the other" do
-      Impl.set(module: DummyModule, key: "test", value: "dogs")
-      Impl.set(module: OtherDummyModule, key: "test", value: "cats")
-      assert Impl.get(module: DummyModule, key: "test", default: nil) == "dogs"
-      assert Impl.get(module: OtherDummyModule, key: "test", default: nil) == "cats"
+      Impl.set(module: DummyModule, key: "test10", value: "dogs")
+      Impl.set(module: OtherDummyModule, key: "test10", value: "cats")
+      assert Impl.get(module: DummyModule, key: "test10", default: nil) == "dogs"
+      assert Impl.get(module: OtherDummyModule, key: "test10", default: nil) == "cats"
     end
   end
 end
